b6ac4ad9f65df0c309b0498e97a27690a04f4999
Vishwam <vishwam.mishra2412@gmail.com>
2026-09-14 17:39:12 +0530

    ------

---
+++ AI/instance.py
@@ -0,0 +1,10 @@
+x = 23
+
+import pandas as pd
+
+x = pd.DataFrame({
+    "name" : ["sam","robin" ,'ram'],
+    "age" : [31,33,21]
+}.index("a","b","c"))
+
+print(x)
--- AI/vectors.py
+++ AI/vectors.py
@@ -1,44 +1,37 @@
-import math 
+from numpy import diff
+from pandas._libs.tslibs.timedeltas import Components
 class Vector:
-    def __init__(self,components):
-        self.components = list(components)
-        self.dim = len(self.components)
+    def __init__(self,component):
+        self.component = component
+        self.dim = len(component)
     
-    def __add__(self,other):
-        return Vector([a+b for a,b in zip(self.components,other.components)])
+    def __sum__(self,other):
+        return [x+y  for x , y in zip(self.component,other.component)]
 
-    def __sub__(self,other):
-        return Vector([a-b for a,b in zip(self.components,other.components)])
+    def __difference__(self,other):
+        return [x-y  for x , y in zip(self.component,other.component)]
+    
+    def __magnitude__(self):
+        return sum(x**2 for x in self.component)**0.5
 
-    def dot(self,other):
-        return sum(a*b for a,b in zip(self.components,other.components))
+    def __normalize__(self):
+        magnitude = self.__magnitude__()
+        return [x/magnitude for x in self.component]
+    
+    def __dot__(self,other):
+        return sum(x*y for x ,y in zip(self.component,other.component))
 
-    def magnitude(self):
-        return sum(x**2 for x in self.components) ** 0.5
-
-    def normalize(self):
-        m = self.magnitude()
-        return Vector([x/m for x in self.components])
-
-    def cosine_similarity(self,other):
-        return self.dot(other) / (self.magnitude() * other.magnitude())
-
+    def __scale__(self,a):
+        return [x*a for x in self.component]
+    
+    def __gram_schmidt__(self,other):
+        z = Vector(self.__normalize__())
+        b = other.__normalize__()
+        n =  z-(b.__scale__(other.__magnitude__()))
+        return n.__normalize__()
+    
     def __repr__(self):
-        return f"Vector({self.components})"
-    
-    def angle_between(self,other):
-        return math.acos(self.cosine_similarity(other))
+        return f"Vector({self.component})"
 
-
-
-if  __name__=="__main__":
-
-    a = Vector([1,2,3])
-    b = Vector([2,34,45])
-
-    print(f"a+b : {a+b}")
-    print(f"a dot b : {a.dot(b)}")
-    print(f"a repr : {a}")
-    print(f"angle : {a.angle_between(b):.3f}")
 
 
